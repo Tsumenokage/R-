e44b5a1d3c6958a7813e86bd1a876da72ce697af
FullyK <mcfvfamily@hotmail.fr>
2015-10-20 11:44:49 +0200

    Pourri

--- Identification.m
+++ Identification.m
@@ -13,7 +13,6 @@ for i=frame:3
    TermeGeneral(:,:,1) = TermeGeneral(:,:,1)-vecteurMoyenne(1);
    TermeGeneral(:,:,2) = TermeGeneral(:,:,2)-vecteurMoyenne(2);
    TermeGeneral(:,:,3) = TermeGeneral(:,:,3)-vecteurMoyenne(3);
-<<<<<<< HEAD
    
 % TermeGeneral = randi(100, 4, 4 ,3) ;
 % disp(TermeGeneral);
@@ -33,40 +32,4 @@ end
 
  imagesc(DistanceMahalanobis), colorbar
  
-=======
-
    
-   [LargeurVideo,HauteurVideo,RGB] = size(CurrentPic);
-   DistanceMahalanobis = zeros(LargeurVideo,HauteurVideo);
-
-
-   for x=1:LargeurVideo
-       for y=1:HauteurVideo
-          TermeMaha=[TermeGeneral(x,y,1),TermeGeneral(x,y,2),TermeGeneral(x,y,3)];
-          TransposeMaha = transpose(TermeMaha);
-          DistanceMahalanobis(x,y) = (TermeMaha) * inv(MatriceCovariance)* (TransposeMaha);
-          
-          
-          if(DistanceMahalanobis(x,y) < Seuil)
-              DistanceMahalanobis(x,y) = 1;
-          else
-              DistanceMahalanobis(x,y) = 0;
-          end;
-        
-       end
-   end
-   MatrixSeuil{i} = DistanceMahalanobis;
-   
-   
-end
-
-figure, imshow(MatrixSeuil{1})
-SE = strel('disk', 2, 4);
-Test = imerode(MatrixSeuil{1},SE);
-Test = imdilate(Test,SE);
-figure, imshow(Test)
-
-L=bwlabel(Test,4);
-
-figure, imagesc(L), colorbar
->>>>>>> origin/master
